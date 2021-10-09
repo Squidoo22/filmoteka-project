45c6a9d0ff58ab283ad3faff34039ae09e30d414
Andrey F <media.andrej@gmail.com>
2021-10-09 20:14:33 +0300

    commit

---
+++ src/js/components/search-by-input.js
@@ -0,0 +1,21 @@
+import { getSearchedMovies } from '../api/movies-api';
+import debounce from 'lodash.debounce';
+
+const { log } = console;
+
+document.querySelector('.js-search-form').addEventListener('input', debounce(onFilterChange, 500));
+
+function onFilterChange(e) {
+    log(e.target.value.toLowerCase())
+    try {
+        getSearchedMovies(e.target.value.toLowerCase())
+            .then(data => {
+                log(data);
+            })
+    } catch (e) {
+        log('error', e);
+    } finally {
+        // block finally
+    }   
+}
+
