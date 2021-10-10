684af63200b8b34e25e653f4f496792bc248112c
Taras-Kolomiiets <taraskolomiiets8991@gmail.com>
2021-10-10 14:46:37 +0300

    Add rendering trending movies + pagination

---
+++ src/js/components/pagination.js
@@ -0,0 +1,54 @@
+import Pagination from 'tui-pagination';
+import { renderTrendingMovies } from './render-trending-movies';
+import { getTrendingMovies } from '../api/movies-api';
+
+const onPageClick = async event => {
+  const renderNewPage = await renderTrendingMovies(event.page);
+  scrollToNewPage();
+  return renderNewPage;
+};
+
+const scrollToNewPage = () => {
+  document.querySelector('.gallery').scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+};
+
+export const createPagination = () => {
+  const container = document.getElementById('tui-pagination-container');
+
+  getTrendingMovies()
+    .then(movies => {
+      const pagination = new Pagination(container, {
+        totalItems: movies.total_results,
+        itemsPerPage: 20,
+        visiblePages: 5,
+        page: 1,
+        centerAlign: true,
+        firstItemClassName: 'tui-first-child',
+        lastItemClassName: 'tui-last-child',
+        usageStatistics: false,
+        template: {
+          page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+          currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+          moveButton:
+            '<a href="#" class="tui-page-btn tui-{{type}}">' +
+            '<span class="tui-ico-{{type}}">{{type}}</span>' +
+            '</a>',
+          disabledMoveButton:
+            '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+            '<span class="tui-ico-{{type}}">{{type}}</span>' +
+            '</span>',
+          moreButton:
+            '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+            '<span class="tui-ico-ellip">...</span>' +
+            '</a>',
+        },
+      });
+      return pagination;
+    })
+    .then(pagination => {
+      pagination.on('beforeMove', onPageClick);
+    });
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 import '../sass/main.scss';
 import { getGenres, getMovieById, getSearchedMovies, getTrendingMovies } from './api/movies-api';
 import './components/search-by-input';
-
+import { renderTrendingMovies } from './components/render-trending-movies';
+import { createPagination } from './components/pagination';
 
 // getSearchedMovies('avengers').then(movies => console.log(movies.results));
 // getTrendingMovies().then(movies => console.log(movies.results));
 // getMovieById(100).then(movie => console.log(movie));
 // getGenres().then(genres => console.log(genres));
+
+renderTrendingMovies();
+createPagination();
