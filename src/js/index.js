40f210e32de76465ce7637e4d140c78649e971df
Taras-Kolomiiets <taraskolomiiets8991@gmail.com>
2021-10-13 21:52:01 +0300

    Add pagination to search

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,12 @@
 import '../sass/main.scss';
 import { getGenres, getMovieById, getSearchedMovies, getTrendingMovies } from './api/movies-api';
 import './components/search-by-input';
-import { renderTrendingMovies } from './components/render-trending-movies';
 import { createPagination } from './components/pagination';
 import './components/our-team-modal';
 import './components/movie-modal';
 import saveGenres from './components/save-genres-to-ls';
-
 import './components/change-home-library';
+import { renderTrendingMovies } from './components/render-trending-movies';
 
 // getSearchedMovies('avengers').then(movies => console.log(movies.results));
 // getTrendingMovies().then(movies => console.log(movies.results));
