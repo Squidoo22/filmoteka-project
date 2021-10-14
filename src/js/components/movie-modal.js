1e9e822b422255c137102a12396274859d08036d
Mikhaylo-Khokhlov <mihailhohlov257@gmail.com>
2021-10-14 16:24:55 +0300

    add remove btn

--- src/js/components/movie-modal.js
+++ src/js/components/movie-modal.js
@@ -41,6 +41,8 @@ const getMovie = async function (id) {
               <p class="modal__desc">${movie.overview}</p>
                 <button type="button" class="modal__button" id="watched">add to Watched</button>
                 <button type="button" class="modal__button modal__button--position" id="queue">add to queue</button>
+                <button type="button" class="modal__button btn__disabled" id="watchedRemove">remove from Watched</button>
+                <button type="button" class="modal__button  modal__button--position btn__disabled" id="queueRemove">remove from queue</button>
                 <button color="black" type="button" class="basicLightbox-bg">
                    
                 </button>
@@ -55,64 +57,79 @@ const getMovie = async function (id) {
     showModal.show();
     if (showModal.show() === true) {
       const watchedBtn = document.querySelector('#watched');
+      const removeWatchedBtn = document.querySelector('#watchedRemove');
       const queueBtn = document.querySelector('#queue');
+      const removeQueueBtn = document.querySelector('#queueRemove');
 
-      addToWatched(movie, watchedBtn);
-      addToQueue(movie, queueBtn);
-      testBtnQueue(movie, queueBtn);
-      testBtnWatch(movie, watchedBtn);
+      addToWatched(movie, watchedBtn, removeWatchedBtn);
+      removeFromWatched(movie, watchedBtn, removeWatchedBtn);
+      addToQueue(movie, queueBtn, removeQueueBtn);
+      testBtnQueue(movie, queueBtn, removeQueueBtn);
+      testBtnWatch(movie, watchedBtn, removeWatchedBtn);
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-const addToWatched = (movie, watchedBtn) => {
+const addToWatched = (movie, watchedBtn, removeWatchedBtn) => {
   watchedBtn.addEventListener('click', () => {
     watchedArr.push(movie);
     localStorage.setItem('watchedMovie', JSON.stringify(watchedArr));
-    testBtnWatch(movie, watchedBtn);
+    testBtnWatch(movie, watchedBtn, removeWatchedBtn);
   });
 };
 
-const addToQueue = (movie, queueBtn) => {
+const removeFromWatched = (movie, watchedBtn, removeWatchedBtn) => {
+  removeWatchedBtn.addEventListener('click', () => {
+    const getWatchedArr = localStorage.getItem('watchedMovie');
+    const storageMovieArr = JSON.parse(getWatchedArr);
+    console.log(storageMovieArr, 'do');
+    const searchId = movie.id;
+    const index = storageMovieArr.findIndex(el => el.id === searchId);
+    const modifiedArr = storageMovieArr.splice(index, 1);
+    console.log(modifiedArr, 'modifiedArr');
+    localStorage.setItem('watchedMovie', JSON.stringify(modifiedArr));
+    testBtnWatch(movie, watchedBtn, removeWatchedBtn);
+  });
+};
+
+const addToQueue = (movie, queueBtn, removeQueueBtn) => {
   queueBtn.addEventListener('click', () => {
     queueArr.push(movie);
     localStorage.setItem('queueMovie', JSON.stringify(queueArr));
-    testBtnQueue(movie, queueBtn);
+    testBtnQueue(movie, queueBtn, removeQueueBtn);
   });
 };
 
-const testBtnWatch = (movie, watchedBtn) => {
+const testBtnWatch = (movie, watchedBtn, removeWatchedBtn) => {
   const getWatchedArr = localStorage.getItem('watchedMovie');
   const storageMovieArr = JSON.parse(getWatchedArr);
 
   for (let i = 0; i < storageMovieArr.length; i += 1) {
     if (storageMovieArr[i].id === movie.id) {
-      watchedBtn.textContent = 'remove from Watched';
       watchedBtn.classList.add('btn__disabled');
-
+      removeWatchedBtn.classList.remove('btn__disabled');
       return;
     } else {
-      watchedBtn.textContent = 'add to Watched';
       watchedBtn.classList.remove('btn__disabled');
+      removeWatchedBtn.classList.add('btn__disabled');
     }
   }
 };
 
-const testBtnQueue = (movie, queueBtn) => {
+const testBtnQueue = (movie, queueBtn, removeQueueBtn) => {
   const getQueuedArr = localStorage.getItem('queueMovie');
   const storageQueueMovieArr = JSON.parse(getQueuedArr);
 
   for (let i = 0; i < storageQueueMovieArr.length; i += 1) {
     if (storageQueueMovieArr[i].id === movie.id) {
-      queueBtn.textContent = 'remove from queue';
       queueBtn.classList.add('btn__disabled');
-
+      removeQueueBtn.classList.remove('btn__disabled');
       return;
     } else {
-      queueBtn.textContent = 'add to queue';
       queueBtn.classList.remove('btn__disabled');
+      removeQueueBtn.classList.add('btn__disabled');
     }
   }
 };
