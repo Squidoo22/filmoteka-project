67a5081ade58eb1f84ebe54e36d50f6359dfd3d1
dkolot <dkolotsoftserveinc.com>
2021-10-19 21:01:36 +0300

    fixes

---
+++ src/js/components/to-top-btn.js
@@ -0,0 +1,19 @@
+const scrollTopBtn = document.querySelector('.scroll-to-top');
+console.log(scrollTopBtn)
+window.addEventListener('scroll', () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+  if (scrollTop > clientHeight) {
+    scrollTopBtn.classList.add('show');
+  } else {
+    scrollTopBtn.classList.remove('show');
+  }
+});
+
+scrollTopBtn.addEventListener('click', scrollToTop);
+
+function scrollToTop() {
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
